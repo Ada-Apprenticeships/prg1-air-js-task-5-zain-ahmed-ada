10ca1c0d10ac4097e8905f14de93d63c883fa693
Zain Ahmed <zain.ahmed@ada.ac.uk>
2024-11-01 16:46:29 +0000

    Added main logic to the calculateIncome function

--- planning.js
+++ planning.js
@@ -1,30 +1,46 @@
-const fs = require('fs');
+const fs = require("fs");
 
-function readCsv(filename, delimiter = ',') {
-    try {
-        const fileContent = fs.readFileSync(filename, { encoding: 'utf-8' });
-        const rows = fileContent.split('\n');
-        const data = [];
+const readCSV = (filename, delimiter = ",") => {
+	try {
+		const fileContent = fs.readFileSync(filename, { encoding: "utf-8" });
+		const rows = fileContent.split("\n");
+		const data = [];
 
-        for (let i = 1; i < rows.length; i++) {
-            const row = rows[i].trim();
-            if (row) {
-                const columns = row.split(delimiter);
-                data.push(columns);
-            }
-        }
+		for (let i = 1; i < rows.length; i++) {
+			const row = rows[i].trim();
+			if (row) {
+				const columns = row.split(delimiter);
+				data.push(columns);
+			}
+		}
+		return data;
+	} catch (err) {
+		console.error("Error reading file:", err.message);
+		return null;
+	}
+};
 
-        return data;
-    } catch (err) {
-        console.error("Error reading file:", err.message);
-        return null;
-    }
-}
+const airportsData = readCSV("airports.csv");
+const aeroplanesData = readCSV("aeroplanes.csv");
+const valid_flight_data = readCSV("valid_flight_data.csv");
+
+const calculateIncome = (inputData) => {
+	inputData.forEach((row) => {
+		let economySeatCount = row[3] * 399;
+		let businessSeatCount = row[4] * 999;
+		let firstClassSeatCount = row[5] * 1899;
+
+		const total = economySeatCount + businessSeatCount + firstClassSeatCount;
+		console.log(total);
+	});
+};
+
+console.log(calculateIncome(aeroplanesData));
 
 // Usage example
-const airportsData = readCsv('airports.csv');
-if (airportsData) {
-    airportsData.forEach(row => {
-        console.log(row);
-    });
-}
+// const airportsData = readCSV("airports.csv");
+// if (airportsData) {
+// 	airportsData.forEach((row) => {
+// 		console.log(row);
+// 	});
+// }
